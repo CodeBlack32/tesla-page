1dc3be4fa670caaf73425fa08957ddf22a6f337f
Jarvis Black <usnblack@gmail.com>
2021-01-17 15:54:01 -0500

    Almost Replicated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,93 @@
 import "./App.css";
 import Header from "./components/Header";
+import Item from "./components/Item";
+
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Code Black</h1>
+      <div className="app_items">
+        <Item
+          title="Lower Cost with Solar"
+          desc="Money Back Guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnText="Order Now"
+          leftBtnLink=""
+          rightBtnText="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Lower Cost with Solar"
+          desc="Money Back Guarantee"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnText="Order Now"
+          leftBtnLink=""
+          rightBtnText="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Lower Cost with Solar"
+          desc="Money Back Guarantee"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnText="Order Now"
+          leftBtnLink=""
+          rightBtnText="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Lower Cost with Solar"
+          desc="Money Back Guarantee"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnText="Order Now"
+          leftBtnLink=""
+          rightBtnText="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Lower Cost with Solar"
+          desc="Money Back Guarantee"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnText="Order Now"
+          leftBtnLink=""
+          rightBtnText="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Lower Cost with Solar"
+          desc="Money Back Guarantee"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnText="Order Now"
+          leftBtnLink=""
+          rightBtnText="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const button = () => {
+  return <div className="button"></div>;
+};
+
+export default button;
